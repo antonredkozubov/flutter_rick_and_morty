aa6fe60f95a49f2ca3637b8f274324beed8f6c2f
Anton Redkozubov <39982533+antonredkozubov@users.noreply.github.com>
2023-03-06 17:24:01 +0300

    Fixed tests

--- lib/features/hero_details/view/bloc/details_state.dart
+++ lib/features/hero_details/view/bloc/details_state.dart
@@ -1,26 +1,5 @@
 part of 'details_bloc.dart';
 
-
-// class DetailsState {
-//   final HeroResultDTO? hero;
-//   final bool isLoading;
-
-//   DetailsState({
-//     this.hero,
-//     this.isLoading = false,
-//   });
-
-//   DetailsState copyWith({
-//     HeroResultDTO? hero,
-//     bool isLoading = false,
-//   }) {
-//     return DetailsState(
-//       hero: hero ?? this.hero,
-//       isLoading: isLoading,
-//     );
-//   }
-// }
-
 @immutable 
 class DetailsState extends Equatable { 
   const DetailsState();
@@ -33,7 +12,4 @@ class CharacterLoaded extends DetailsState {
   final HeroResultDTO hero;
 
   const CharacterLoaded({required this.hero});
-
-  @override
-  List<Object?> get props => [hero];
 }
--- lib/features/heroes_list/view/bloc/heroes_state.dart
+++ lib/features/heroes_list/view/bloc/heroes_state.dart
@@ -1,25 +1,5 @@
 part of 'heroes_bloc.dart';
 
-// class HeroesState {
-//   final List<HeroResultDTO> heroes;
-//   final bool isLoading;
-
-//   HeroesState({
-//     this.heroes = const [],
-//     this.isLoading = false,
-//   });
-
-//   HeroesState copyWith({
-//     List<HeroResultDTO>? heroes,
-//     bool isLoading = false,
-//   }) {
-//     return HeroesState(
-//       heroes: heroes ?? this.heroes,
-//       isLoading: isLoading,
-//     );
-//   }
-// }
-
 @immutable 
 class HeroesState extends Equatable { 
   const HeroesState();
@@ -32,7 +12,4 @@ class CharactersLoaded extends HeroesState {
   final List<HeroResultDTO>? heroes;
 
   const CharactersLoaded({required this.heroes});
-
-  @override
-  List<Object?> get props => [heroes];
 }
